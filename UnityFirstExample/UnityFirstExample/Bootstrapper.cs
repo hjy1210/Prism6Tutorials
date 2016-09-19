109682e29a3b13c5ee7c747849cc36c18d92c8c8
Hon-Jang Yang <hjyanghj@gmail.com>
2016-09-19 17:19:59 +0800

    Add UnityFirstExample
    Visual Studio Solution

---
+++ UnityFirstExample/UnityFirstExample/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using UnityFirstExample.Views;
+using System.Windows;
+
+namespace UnityFirstExample
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
